b629a43e242f621c015fc03d1d9c48457c9dfc26
xuxueli <931591021@qq.com>
2019-01-16 19:14:46 +0800

    json tool optimize

--- xxl-registry-client/src/main/java/com/xxl/registry/client/util/json/BasicJson.java
+++ xxl-registry-client/src/main/java/com/xxl/registry/client/util/json/BasicJson.java
@@ -1,10 +1,9 @@
 package com.xxl.registry.client.util.json;
 
-import com.xxl.registry.client.util.FieldReflectionUtil;
-
-import java.lang.reflect.Field;
-import java.lang.reflect.ParameterizedType;
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author xuxueli 2018-11-30
@@ -12,13 +11,64 @@ import java.util.*;
 public class BasicJson {
 
 
-    // ---------------------- inner method ----------------------
-
     private static final BasicJsonReader basicJsonReader = new BasicJsonReader();
     private static final BasicJsonwriter basicJsonwriter = new BasicJsonwriter();
 
 
-    // parse biz-object from map-object
+    /**
+     * object to json
+     *
+     * @param object
+     * @return
+     */
+    public static String toJson(Object object) {
+        return basicJsonwriter.toJson(object);
+    }
+
+    /**
+     * parse json to map
+     *
+     * @param json
+     * @return      only for filed type "null、ArrayList、LinkedHashMap、String、Long、Double、..."
+     */
+    public static Map<String, Object> parseMap(String json) {
+        return basicJsonReader.parseMap(json);
+    }
+
+    /**
+     * json to List
+     *
+     * @param json
+     * @return
+     */
+    public static List<Object> parseList(String json) {
+        return basicJsonReader.parseList(json);
+    }
+
+
+
+    public static void main(String[] args) {
+        Map<String, Object> result = new HashMap<>();
+        result.put("code", 200);
+        result.put("msg", "success");
+        result.put("arr", Arrays.asList("111","222"));
+        result.put("float", 1.11f);
+        result.put("temp", null);
+
+        String json = toJson(result);
+        System.out.println(json);
+
+        Map<String, Object> mapObj = parseMap(json);
+        System.out.println(mapObj);
+
+        List<Object> listInt = parseList("[111,222,33]");
+        System.out.println(listInt);
+
+    }
+
+
+
+    /*// parse biz-object from map-object
     private static <T> T parseBizObjectFromMapObject(final Map<String, Object> mapObject, Class<T> businessClass){
         // parse class (only first level)
         try {
@@ -111,27 +161,14 @@ public class BasicJson {
     }
 
 
-    // ---------------------- tool method ----------------------
-
-    /**
-     * object to json
-     *
-     * @param object
-     * @return
-     */
-    public static String toJson(Object object) {
-        return basicJsonwriter.toJson(object);
-    }
-
-
-    /**
+    *//**
      * parse json to <T>
      *
      * @param json
      * @param businessClass     null for base-class "Integer、Long、Map ... " , other for business-class
      * @param <T>
      * @return
-     */
+     *//*
     public static <T> T parseObject(String json, Class<T> businessClass) {
 
         // map object
@@ -146,25 +183,14 @@ public class BasicJson {
         }
     }
 
-    /**
-     * parse json to map
-     *
-     * @param json
-     * @return      only for filed type "null、ArrayList、LinkedHashMap、String、Long、Double、..."
-     */
-    public static Map<String, Object> parseMap(String json) {
-        return parseObject(json, null);
-    }
-
-
-    /**
+    *//**
      * json to List<T>
      *
      * @param json
      * @param businessClass     null for base-class "Integer、Long、Map ... " , other for business-class
      * @param <T>
      * @return
-     */
+     *//*
     public static <T> List<T> parseList(String json, Class<T> businessClass) {
 
         // list object
@@ -177,37 +203,9 @@ public class BasicJson {
             return parseBizObjectListFromMapList(listObject, businessClass);
         }
 
-    }
-
-    /**
-     * json to List
-     *
-     * @param json
-     * @param <T>
-     * @return
-     */
-    public static <T> List<T> parseList(String json) {
-        return parseList(json, null);
-    }
+    }*/
 
 
-    public static void main(String[] args) {
-        Map<String, Object> result = new HashMap<>();
-        result.put("code", 200);
-        result.put("msg", "success");
-        result.put("arr", Arrays.asList("111","222"));
-        result.put("float", 1.11f);
-        result.put("temp", null);
 
-        String json = toJson(result);
-        System.out.println(json);
-
-        Map<String, Object> mapObj = parseMap(json);
-        System.out.println(mapObj);
-
-        List<Integer> listInt = parseList("[111,222,33]", null);
-        System.out.println(listInt);
-
-    }
 
 }
